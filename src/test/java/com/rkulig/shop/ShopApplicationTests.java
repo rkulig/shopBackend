f76e25ae4cf26251c28dd56e608459d40d997d1c
Radoslaw_Kulig <Radoslaw_Kulig@epam.com>
2022-11-08 18:59:26 +0100

    init

---
+++ src/test/java/com/rkulig/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkulig.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

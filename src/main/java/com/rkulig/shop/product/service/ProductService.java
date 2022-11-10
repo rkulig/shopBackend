5f50f21a8bf73d6a711205be560a3206c5a04b37
Radoslaw_Kulig <Radoslaw_Kulig@epam.com>
2022-11-10 17:28:27 +0100

    pagination of products

--- src/main/java/com/rkulig/shop/product/service/ProductService.java
+++ src/main/java/com/rkulig/shop/product/service/ProductService.java
@@ -3,17 +3,17 @@ package com.rkulig.shop.product.service;
 import com.rkulig.shop.product.model.Product;
 import com.rkulig.shop.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository prductRepository;
-    public List<Product> getProducts() {
-        return prductRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return prductRepository.findAll(pageable);
     }
 
 }
